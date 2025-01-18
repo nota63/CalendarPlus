3615370a90525aeddabae2802a8f136ec6db9ad4
nota63 <vishaldudhabarve105@gmail.com>
2025-01-18 15:00:02 +0530

    updated database- postgresql

--- ch/build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# build_files.sh
-#!/bin/bash
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
-
-echo "Cleaning up migrations (optional)..."
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc" -delete
