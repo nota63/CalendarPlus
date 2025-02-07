ab42e36819504ac770fb6675cf677d5de464b2ad
nota63 <vishaldudhabarve105@gmail.com>
2025-02-07 13:01:36 +0530

    added search bar & display online popeles in workspace

--- ch/organizations/views.py
+++ ch/organizations/views.py
@@ -976,6 +976,15 @@ def organization_pulse_view(request, org_id):
     if not Profile.objects.filter(organization=organization).exists():
         return render(request, "pulse.html", {"error": "No members found in this organization."})
 
+        # Time slots for "People Online" section
+    time_slots = [
+        "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM",
+        "8 AM", "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM",
+        "4 PM", "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM"
+    ] 
+
     activity_data = get_last_two_activities(org_id)
     
-    return render(request, "organizations/pulse/pulse.html", {"activity_data": activity_data})
+    return render(request, "organizations/pulse/pulse.html", {"activity_data": activity_data,'organization':organization,'profile':Profile,'time_slots':time_slots})
+
+
