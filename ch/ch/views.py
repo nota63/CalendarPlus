ff8ffeb6af49fb51e035201f60a579061ce6f9bc
nota63 <vishaldudhabarve105@gmail.com>
2025-03-15 21:21:32 +0530

    fixed middleware

--- ch/ch/views.py
+++ ch/ch/views.py
@@ -94,14 +94,15 @@ class OrgGuideView(View):
 
 
 # FETCH UNSEEN REWARDS 
-from group_tasks.models import PendingRewardNotification
+from group_tasks.models import PendingRewardNotification, CalPoints
 
 @login_required
 def latest_pending_reward(request):
     latest_reward = PendingRewardNotification.objects.filter(user=request.user, is_seen=False).order_by('-created_at').first()
-    
+    calpoints=CalPoints.objects.filter(task=latest_reward.task,organization=latest_reward.organization,group=latest_reward.group,user=request.user).first()
     if latest_reward:
         latest_reward.is_seen = True  
         latest_reward.save()
+    
 
-    return render(request, 'task/rewards/latest_rewards.html', {'latest_reward': latest_reward})
+    return render(request, 'task/rewards/latest_rewards.html', {'latest_reward': latest_reward,'calpoints':calpoints})
