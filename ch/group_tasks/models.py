b20b416832d11114b8459e905bbb4e14b28dcba6
nota63 <vishaldudhabarve105@gmail.com>
2025-03-19 20:20:16 +0530

    ui - automate escalation if stalle

--- ch/group_tasks/models.py
+++ ch/group_tasks/models.py
@@ -152,6 +152,7 @@ class Task(models.Model):
     task_chaining_notification_sent=models.BooleanField(default=False, null=True, blank=True) 
     install_new_app_notification=models.BooleanField(default=False, null=True, blank=True)
     deadline_extend_notification=models.BooleanField(default=False, null=True, blank=True)
+    automate_stalled_action=models.BooleanField(default=False, null=True, blank=True)
 
 
 
@@ -911,6 +912,7 @@ class AutomationTask(models.Model):
     assign_task_if_previous_completed=models.BooleanField(default=False)
     install_new_apps=models.BooleanField(default=False,null=True,blank=True)
     extend_deadline=models.BooleanField(default=False, null=True, blank=True)
+    automate_stalled=models.BooleanField(default=True, null=True, blank=True)
     # track automations
     last_executed = models.DateTimeField(null=True, blank=True)
     processed_tasks = models.ManyToManyField(Task, blank=True, related_name="processed_automations")
