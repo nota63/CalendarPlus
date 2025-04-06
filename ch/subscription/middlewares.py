d5e94c891e4df3f494541bc3c9a2ea68d12e947e
nota63 <vishaldudhabarve105@gmail.com>
2025-04-06 22:45:14 +0530

    save session time in localstorage

--- ch/subscription/middlewares.py
+++ ch/subscription/middlewares.py
@@ -2,11 +2,34 @@ from django.conf import settings
 import logging
 logger = logging.getLogger(__name__)
 
-class ImpersonationMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+# class ImpersonationMiddleware:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
 
-    def __call__(self, request):
+#     def __call__(self, request):
+#         logger.debug("🔥 ImpersonationMiddleware executing...")
+
+#         impersonator_id = request.session.get('impersonator_id')
+#         impersonator_name = request.session.get('impersonator_name')
+
+#         logger.debug(f"Impersonator ID: {impersonator_id}")
+#         logger.debug(f"Impersonator Name: {impersonator_name}")
+
+#         if impersonator_id:
+#             request.impersonating = True
+#             request.impersonator_name = impersonator_name
+#         else:
+#             request.impersonating = False
+#             request.impersonator_name = None
+
+#         response = self.get_response(request)
+#         return response
+
+
+from django.utils.deprecation import MiddlewareMixin
+from django.template.loader import render_to_string
+class ImpersonationMiddleware(MiddlewareMixin):
+    def process_request(self, request):
         logger.debug("🔥 ImpersonationMiddleware executing...")
 
         impersonator_id = request.session.get('impersonator_id')
@@ -22,5 +45,21 @@ class ImpersonationMiddleware:
             request.impersonating = False
             request.impersonator_name = None
 
-        response = self.get_response(request)
+    def process_response(self, request, response):
+        if (
+            getattr(request, 'impersonating', False) and
+            'text/html' in response.get('Content-Type', '') and
+            hasattr(response, 'content')
+        ):
+            try:
+                banner_html = render_to_string("partials/impersonation_alert.html", {"request": request})
+                content = response.content.decode("utf-8")
+
+                if "</body>" in content:
+                    logger.debug("✨ Injecting impersonation banner into HTML...")
+                    content = content.replace("</body>", banner_html + "</body>")
+                    response.content = content.encode("utf-8")
+            except Exception as e:
+                logger.error(f"💥 Failed to inject impersonation banner: {e}")
+
         return response
